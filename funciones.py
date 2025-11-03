95ad67444d736837feabe8528f4cf9f7b77971de
Misol <a01664919@tec.mx>
2025-11-02 21:07:59 -0600

    Agregar módulo funciones.py con cálculo de áreas

---
+++ funciones.py
@@ -0,0 +1,17 @@
+
+
+
+import math
+
+def triangulo(base, altura):
+    area = (base * altura) / 2
+    return area
+
+def rectangulo(base, altura):
+    area = base * altura
+    return area
+
+def circulo(radio):
+    area = math.pi * (radio ** 2)
+    return area
+
